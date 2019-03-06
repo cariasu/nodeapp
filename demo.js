4e5b1362335cbbf5cd919e4e1cae6855dd75c279
Andres <carias@soaint.com>
2019-03-06 05:41:09 +0000

    master: R1

--- demo.js
+++ demo.js
@@ -1,1 +1,12 @@
 console.log('Hello world!');
+const http = require('http');
+
+const server = http.createServer((request, response) =>
+{
+    console.log('Recibiendo peticiones');
+    response.write('<h1>Hola mundo</h1>');
+    response.end;
+})
+
+server.listen(5555);
+console.log('Servidor levantado');
